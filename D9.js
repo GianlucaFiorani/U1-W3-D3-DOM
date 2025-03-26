f4ab70fbfbf43dddef4a92245eef5febbdce7bf3
Gianluca Fiorani <gianluca.fiorani97@gmail.com>
2025-03-26 19:30:37 +0100

    add all files

--- D9.js
+++ D9.js
@@ -7,7 +7,7 @@ form.onsubmit = function (e) {
   const deleteBt = document.createElement("button");
   toDoLi.classList.add("line");
   toDoLi.innerText = toDoImput.value;
-  deleteBt.innerText = "cancella";
+  deleteBt.innerText = "X";
 
   deleteBt.onclick = function (e) {
     e.currentTarget.closest(".line").remove();
@@ -18,15 +18,17 @@ form.onsubmit = function (e) {
   };
   toDoLi.append(deleteBt);
   list.append(toDoLi);
+  toDoImput.value = "";
 };
 
 const changeColorWithRandom = function () {
   const h1 = document.querySelector("h1");
-  setTimeout(() => {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
-    h1.style.color = `rgb( ${r} ${g} ${b})`;
-  }, 100);
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  h1.style.color = `rgb( ${r} ${g} ${b})`;
 };
-changeColorWithRandom();
+
+setInterval(() => {
+  changeColorWithRandom();
+}, 450);
